ceb847e14ff90b4746843dec269e4642ced621b5
John Morris <2e0byo@gmail.com>
2026-07-02 11:11:50 +0200

    fix: pick system theme on startup.

--- config.el
+++ config.el
@@ -32,10 +32,15 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
 (setq custom-safe-themes
       '("2b0fcc7cc9be4c09ec5c75405260a85e41691abb1ee28d29fcd5521e4fca575b"
         "d97ac0baa0b67be4f7523795621ea5096939a47e8b46378f79e78846e0e4ad3d" default))
+
+(if (string-match-p "light" (with-temp-buffer
+                              (insert-file-contents "~/.cache/system-theme/theme")
+                              (buffer-string)))
+    (setq doom-theme 'solarized-light)
+  (setq doom-theme 'solarized-dark))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
